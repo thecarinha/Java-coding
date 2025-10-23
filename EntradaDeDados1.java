dd30541e6ecc02609367ebda1c29680924afd782
thecarinha <joaolima15257@gmail.com>
2025-10-22 21:13:31 -0400

    Delete EntradaDeDados1.java

--- EntradaDeDados1.java
+++
@@ -1,8 +0,0 @@
-public class EntradaDeDados1{
-public static void main(String[]args){
-int n;
-System.out.println("digite um numero: ");
-n = Integer.parseInt(System.console().readLine());
-System.out.printf("\nvoce digitou: %d",n);
-}
-} 
