2d3bdf1276cca6af94233504191b9318261747de
thecarinha <joaolima15257@gmail.com>
2025-10-22 21:31:18 -0400

    Create EntradaESaidaDeDados.java

---
+++ EntradaESaidaDeDados.java
@@ -0,0 +1,9 @@
+public class EntradaESaidaDeDados{
+public static void main(String[]args){
+	int n;
+	System.out.println("informe um numero(inteiro): ");
+	n = Integer.parseInt(System.console().readLine());
+	System.out.printf("\nvoce digitou: %d",n);
+}
+}
+
